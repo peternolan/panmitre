cf9cde801004884418afe0b3016840449533b626
peternolan <pn7925@gmail.com>
2019-04-07 22:38:06 -0400

    Added query and connected to database. Now database contains csv data

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var http = require('http')
 var express = require('express');
 var path = require('path');
 var bodyParser = require("body-parser");
-var bcrypt = require('bcrypt');
 var app = express();
+var pg = require('pg');
+var conString = "postgres://testing:test@localhost:5432/mitreDB";
+
+var client = new pg.Client(conString);
+client.connect();
 
 
 //server.listen(process.env.PORT || port);
@@ -17,6 +21,17 @@ app.listen(port, () => console.log('listening on ' + port));
 app.get('/', function (req, res) {
 
   res.sendFile(path.join(__dirname + '/public/frontPage.html'));
+
+  console.log('AFTER sendFile');
+
+  client.query('SELECT SELECT DISTINCT code FROM medication' +
+                ' WHERE patient = \'71949668-1c2e-43ae-ab0a-64654608defb\'', (err, results) => {
+    if (err) {
+      throw err
+    }
+
+    console.log("console logging "+ JSON.stringify(results.rows));
+  })
 
 });
 
